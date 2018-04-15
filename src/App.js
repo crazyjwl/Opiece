f06ff9c1b8299419bb962d2473ae3cb7608c004c
crazyjwl <crazyjwl.163.com>
2018-04-15 18:08:25 +0800

    Add Titile and SearchBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { friends } from './friends';
+import SearchBox from './SearchBox';
 
-const App = () => {
-  return (
-    <CardList friends={friends} />
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends : friends,
+      searchfield : '',
+    }
+  }
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value});
+  }
+  render(){
+    const filteredFriends = this.state.friends.filter(friend =>{
+      return friend.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h2>One Piece Friends</h2>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList friends={filteredFriends} />
+      </div>
+
+    );
+  }
 }
 
 export default App;
